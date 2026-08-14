dd276da8f8c4dbe9a3f265cb19ae331baa8f277d
JellehBelleh <mingxuandidi@gmail.com>
2026-08-14 12:08:31 +0800

    Add greet and exit

--- src/main/java/Ubis.java
+++ src/main/java/Ubis.java
@@ -1,10 +1,26 @@
 public class Ubis {
     public static void main(String[] args) {
+        welcome();
+        exit();
+    }
+
+    // Welcomes the user with print messages
+    private static void welcome() {
         String banner = " _   _ ____ ___ ____  \n"
                 + "| | | | __ )|_ _/ ___| \n"
                 + "| | | |  _ \\ | |\\___ \\ \n"
                 + "| |_| | |_) || | ___) |\n"
                 + " \\___/|____/|___|____/ \n";
+
+        System.out.println("_".repeat(30));
         System.out.println(banner);
+        System.out.println("Hello! I am Ubis.");
+        System.out.println("What can I do for you?");
+        System.out.println("_".repeat(30));
+    }
+
+    // Handle exit of the bot, for now just print a message
+    private static void exit() {
+        System.out.println("Goodbye. See you soon!");
     }
 }
