f3096a39de3a08650411f7295665d805e60f6a7c
JellehBelleh <mingxuandidi@gmail.com>
2026-08-14 13:35:06 +0800

    Update Task and TaskList to handle Mark as Done

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,13 +1,23 @@
 // Class that represents a task
 public class Task {
     private String name;
+    private boolean done;
 
     public Task(String name) {
         this.name = name;
+        this.done = false;
+    }
+
+    public void mark() {
+        this.done = true;
+    }
+
+    public void unmark() {
+        this.done = false;
     }
 
     @Override
     public String toString() {
-        return this.name;
+        return "[" + (this.done ? "X" : " ") + "] " + this.name;
     }
 }
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -19,4 +19,28 @@ public class TaskList {
             System.out.println((i + 1) + ": " + tasks.get(i));
         }
     }
+
+    public void markTask(int index) {
+        if (index < 1 || index > tasks.size()) {
+            System.out.println("Sorry, there is no task number " + index + ". Please try again.");
+            return;
+        }
+
+        Task task = tasks.get(index - 1);
+        task.mark();
+        System.out.println("Nice! I've marked this task as DONE:");
+        System.out.println("  " + task);
+    }
+
+    public void unmarkTask(int index) {
+        if (index < 1 || index > tasks.size()) {
+            System.out.println("Sorry, there is no task number " + index + ". Please try again.");
+            return;
+        }
+
+        Task task = tasks.get(index - 1);
+        task.unmark();
+        System.out.println("Okay, I've marked this task NOT done yet:");
+        System.out.println("  " + task);
+    }
 }
