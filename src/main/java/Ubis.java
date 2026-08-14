4800382f039b155a7f59f4c794dc560f2b83f3c9
JellehBelleh <mingxuandidi@gmail.com>
2026-08-14 13:36:13 +0800

    Rename and refactor receiveCommand and handleCommand to handle mark and unmark operation

--- src/main/java/Ubis.java
+++ src/main/java/Ubis.java
@@ -24,7 +24,7 @@ public class Ubis {
 
         // Keep handling commands. Exits when user inputs "bye"
         while (true) {
-            handleCommand(receiveCommand());
+            handleInput(receiveInput());
         }
     }
 
@@ -43,20 +43,61 @@ public class Ubis {
     }
 
     // Method to get a line of input from the user.
-    private static String receiveCommand() {
+    private static String receiveInput() {
         String command = SCANNER.nextLine();
         printDashLine();
         return command;
     }
 
-    // Handles the incoming command
-    private static void handleCommand(String command) {
+    // Handles the incoming input
+    private static void handleInput(String input) {
+        if (input.isEmpty()) {
+            System.out.println("Hi! You can type in a task name and I will keep track of it for you!");
+            System.out.println("type \"help\" for information on commands.");
+
+            return;
+        }
+
+        String[] parts = input.split(" ", 2);
+        String command = null;
+        String argument = null;
+
+        if (parts.length > 0) {
+            command = parts[0];
+        }
+        if (parts.length > 1) {
+            argument = parts[1];
+        }
+
         switch (command) {
             case "bye":
                 exit();
                 break;
             case "list":
                 taskList.listTasks();
+                break;
+            case "help":
+                System.out.println("Here are some commands: ");
+                System.out.println("  list - list all tasks");
+                System.out.println("  mark n - mark the nth task as done");
+                System.out.println("  unmark n - mark the nth task as NOT done");
+                System.out.println("  bye - exit the program");
+                break;
+            case "mark":
+                if (argument == null) {
+                    System.out.println("Please add the task number you want to mark!");
+                    System.out.println("Example: \"mark 4\" if you want to mark the fourth task.");
+                } else {
+                    taskList.markTask(Integer.parseInt(argument));
+                }
+                break;
+            case "unmark":
+                if (argument == null) {
+                    System.out.println("Please add the task number you want to unmark!");
+                    System.out.println("Example: \"unmark 4\" if you want to unmark the fourth task.");
+                } else {
+                    taskList.unmarkTask(Integer.parseInt(argument));
+                }
                 break;
             default:
                 taskList.addTask(command);
